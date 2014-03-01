300ae7477765047210d5c6d034e1d59a0ca36aed
Wai Ho <waiholi@gmail.com>
2014-03-02 01:40:22 +1100

    Added license (BSD 2-clause) to main.cpp and seperate .txt

--- dumpK4W/main.cpp
+++ dumpK4W/main.cpp
@@ -1,3 +1,40 @@
+/*
+Kinect for Windows v2 (ToF sensor) raw data dumper. Takes ~150MB/s of depth, 
+infrared and color frames from the sensor to RAM for a fixed number of frames. 
+It then dumps frames in RAM to HDD. Note that the program is multi-threaded (1 thread per sensor).
+
+Copyright (c) 2014, Wai Ho Li
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Uses OpenCV, which uses the BSD 3-Clause license. For more details, see: 
+http://opencv.org/
+
+Disclaimer (MS K4W v2 SDK):
+This is based on preliminary software and/or hardware, subject to change.
+
+*/
+
 #include <Kinect.h>
 #include <iostream>
 #include <sstream>
